e58015b443967932131285dc537a44c52b628189
3-126-6-PC <you@example.com>
2025-05-05 11:57:30 +1200

    05.05.25

---
+++ 6-Ваня готовый сайт/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("send_to_support").addEventListener('click', function() {
+    alert("Сообщение отправлено!");
+});;
+    
+const btn_1 = document.getElementById("uznat").addEventListener("click", function(){
+    alert('вы узнали всё');
+});
